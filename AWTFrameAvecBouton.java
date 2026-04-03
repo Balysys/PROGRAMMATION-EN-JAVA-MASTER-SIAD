29ca277d6b83d9b64ef772f4c06b753def542873
BalySys <133960894+Balysys@users.noreply.github.com>
2026-04-03 18:37:46 +0000

    VER2

--- AWTFrameAvecBouton.java
+++ AWTFrameAvecBouton.java
@@ -1,25 +1,7 @@
-import java.awt.*;
-
-public class AWTFrameAvecBouton extends Frame {
-    
-    public AWTFrameAvecBouton() {
-        // Créer un bouton
-        Button bt = new Button("Fermer");
-        
-        // Ajouter le bouton à la fenêtre
-        this.add(bt);
-        
-        // Définir les dimensions
-        this.setSize(300, 150);
-        
-        // Donner un titre
-        this.setTitle("Frame AWT avec un bouton");
-        
-        // Rendre visible
-        this.setVisible(true);
-    }
-    
-    public static void main(String[] args) {
-        new AWTFrameAvecBouton();
-    }
+public class First 
+{ 
+public static void main(String[] args) 
+{ 
+System.out.println("First Java application"); 
+} 
 }
