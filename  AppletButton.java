07ce13c9e36b36577caacfd8c9e9f8593d1c2d15
BalySys <133960894+Balysys@users.noreply.github.com>
2026-04-04 12:25:40 +0000

    vk

---
+++  AppletButton.java
@@ -0,0 +1,11 @@
+Import java.applet.*; 
+Import java.awt.*; 
+Public class AppletButton extends Applet  
+{ 
+   Button b = newButton("OK"); 
+publicvoidinit()  
+   { 
+super.init(); 
+this.add(b); 
+   } 
+} 
